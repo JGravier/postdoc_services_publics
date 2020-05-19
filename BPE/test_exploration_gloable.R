02c0d1c47eebabdb6dbd9eeb9fa3ced1405e508a
Julie Gravier <JGravier@users.noreply.github.com>
2020-05-19 16:40:27 +0200

    update with interpo

--- BPE/test_exploration_gloable.R
+++ BPE/test_exploration_gloable.R
@@ -6,6 +6,7 @@ library(ade4)
 library(explor)
 library(Cairo)
 library(GGally)
+library(areal) # interpolation
 
 
 source("fonctions_bases.R")
@@ -530,4 +531,206 @@ tab_acp_test_2 <- sf_services_publics_aires_urbaines %>%
 tab_acp_test_2 %>% select(pop_75_99:densite_2009) %>% ggpairs()
 
 rm(tab_densite, tab_acp_test_2)
+
+# ------ avec interpolation ------------
+# période 2009-2018
+tab_acp_test <- sf_services_publics_aires_urbaines %>%
+  filter(validite_temporelle == "2009-2013-2018") %>%
+  select(AU2010, LIBAU2010, pop2009, pop1999, pop1975, annee, typologie, nb_equip, geometry) %>%
+  group_by(AU2010, LIBAU2010, pop2009, pop1999, pop1975, geometry, annee) %>%
+  summarise_if(is.numeric, sum) %>%
+  pivot_wider(id_cols = AU2010:geometry, names_from = annee, values_from = nb_equip) %>%
+  rename(nb_equip_2009 = `2009`,
+         nb_equip_2013 = `2013`,
+         nb_equip_2018 = `2018`) %>%
+  mutate(densite_2009 = nb_equip_2009/pop2009*10000) %>%
+  mutate(pop_75_99 = TCAM(datefin = pop1999, datedebut = pop1975, nbannee = 24),
+         pop_99_09 = TCAM(datefin = pop2009, datedebut = pop1999, nbannee = 20),
+         sp_09_18 = TCAM(datefin = nb_equip_2013, datedebut = nb_equip_2009, nbannee = 9)) %>%
+  ungroup()
+
+
+# il y a deux AU avec NA dans les services publics (suppression)
+tab_acp_test <- tab_acp_test %>%
+  filter(!is.na(densite_2009) & !is.na(sp_09_18))
+
+tab_acp_test_sf <- tab_acp_test %>%
+  st_as_sf(wkt = "geometry", crs = 2154)
+  
+grid_hexa <- st_make_grid(x = tab_acp_test_sf, 
+                          cellsize = 100*100, # mesures selon le système de projection
+                          square = FALSE, # if FALSE : create hexagonal
+                          what = "centers")
+ggplot() +
+  geom_sf(data = tab_acp_test_sf, fill = "grey90", color = "grey85", size = 0.2) +
+  geom_sf(data = grid_hexa) +
+  ggspatial::annotation_scale(location = "bl",  width_hint = 0.3)
+
+grid_hexa <- grid_hexa %>%
+  st_as_sf() %>% # transfo en sf (avant en sfc)
+  mutate(id = seq(from = 1, to = length(grid_hexa), 1)) %>% # ajout d'une colonne d'id nécessaire pour la fonction aw_interpolate
+  st_set_precision(1000000) %>% # probleme de géometrie initialement : voir https://github.com/r-spatial/sf/issues/603
+  lwgeom::st_make_valid()
+
+interpo_grid_2 <- aw_interpolate(.data = grid_hexa, tid = id, # la grille consituée
+                                         source = tab_acp_test_sf, sid = AU2010, # les données à interpoler
+                                         weight = "sum", # défaut en internsive (not extensive) interpolation
+                                         output = "sf", intensive = c("densite_2009", "pop_75_99", "pop_99_09", "sp_09_18"))
+
+
+
+acp_tableau <- interpo_grid_2 %>% st_drop_geometry() %>% select(densite_2009:sp_09_18)
+
+GGally::ggpairs(acp_tableau %>% filter(sp_09_18 < 10))
+
+acp <- dudi.pca(acp_tableau, scannf = FALSE, nf = ncol(acp_tableau))
+explor(acp)
+
+# ------------------ cah : tab général ------------------------------------
+cah_acp <- acp_tableau %>%
+  CAH_sur_coord_ACP()
+
+plot(cah_acp, hang = -1, cex = 0.6, 
+     main = "Dendro",
+     xlab = "villes")
+
+inertie <- sort(cah_acp$height, decreasing = TRUE)
+plot(inertie, type = "h", xlab = "nombre de classes", ylab = "inertie")
+
+inertie <- inertie/sum(inertie)*100
+barplot(inertie[1:20], col = "#454847", border = "#454847", names.arg = seq(0, 19, 1),
+        xlab = "nombre de classes",
+        ylab = "part de l'inertie totale (%)")
+# donc pour le présent cas, on peut découper en 6 classes (une classe qui contiendra 2 aires urbaines fufu)
+
+typo_tab_general <- cutree(cah_acp, k = 6)  # k = nombre de classes
+
+acp_tableau_enrichi <- acp_tableau %>%
+  Standar() %>% # nécessaire de standardiser les valeurs du tableau initial
+  as_tibble() %>%
+  mutate(cluster = factor(typo_tab_general, levels = 1:6))
+
+acp_tableau_visu <- acp_tableau_enrichi %>% # nouvelle colonne avec appartenance des lignes i par classe
+  group_by(cluster) %>%
+  summarise_if(is.numeric, mean)
+
+a <- acp_tableau_visu %>%
+  rename(`densité en 2009` = densite_2009,
+         `evo. sp 2009-2018` = sp_09_18,
+         `evo. pop. 1975-1999` = pop_75_99,
+         `evo. pop. 1999-2009` = pop_99_09) %>%
+  pivot_longer(cols = -cluster, names_to = "variables", values_to = "moyenne_cluster") %>%
+  ggplot() +
+  geom_bar(aes(x = moyenne_cluster, y = variables, fill = cluster), stat = "identity", show.legend = FALSE) +
+  scale_fill_tableau(palette = "Tableau 10") +
+  xlab("Moyennes des valeurs standardisées par classe") +
+  theme_julie() +
+  theme(axis.title.y = element_blank()) +
+  labs(caption = "J. Gravier 2020 | LabEx DynamiTe, UMR Géographie-cités\nSources: BPE 2009, 2013, 2018 (Insee), délim. AU 2010 géo. 2019 (Insee), ADMIN EXPRESS géo. 2019 (IGN)",
+       subtitle = "Classe de périodes : CAH (distance euclidienne)") +
+  facet_wrap(~cluster)
+
+# carto
+acp_tableau_enrichi <- acp_tableau_enrichi %>%
+  bind_cols(interpo_grid_2 %>% 
+              mutate(geom = x) %>%
+              st_drop_geometry()) %>%
+  st_as_sf(crs = 2154)
+
+b <- ggplot() +
+  geom_sf(data = france, fill = "grey98", color = "grey50", size = 0.3) +
+  geom_sf(data = acp_tableau_enrichi, aes(fill = cluster), show.legend = FALSE, size = 0.2) +
+  scale_fill_tableau(palette = "Tableau 10") +
+  ggspatial::annotation_scale(location = "tr",  width_hint = 0.2) +
+  theme_igray() +
+  theme(axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank()) +
+  ggtitle("Caractérisation de l'évolution des services publics des aires urbaines")
+
+b / a + plot_layout(heights = c(2, 1))
+
+ggsave(filename = "aires_urbaines_acp_interpolation.png", plot = last_plot(), type = "cairo",
+       path = "BPE/sorties/acp_cah/figures/toutes_au", dpi = 300,  width = 25, height = 30, units = "cm")
+
+
+#--------- changements ------------
+acp_tableau <- interpo_grid_2 %>% 
+  st_drop_geometry() %>% 
+  select(densite_2009:sp_09_18) %>%
+  filter(sp_09_18 != 0)
+
+acp <- dudi.pca(acp_tableau, scannf = FALSE, nf = ncol(acp_tableau))
+explor(acp)
+
+cah_acp <- acp_tableau %>%
+  CAH_sur_coord_ACP()
+
+plot(cah_acp, hang = -1, cex = 0.6, 
+     main = "Dendro",
+     xlab = "villes")
+
+inertie <- sort(cah_acp$height, decreasing = TRUE)
+plot(inertie, type = "h", xlab = "nombre de classes", ylab = "inertie")
+
+inertie <- inertie/sum(inertie)*100
+barplot(inertie[1:20], col = "#454847", border = "#454847", names.arg = seq(0, 19, 1),
+        xlab = "nombre de classes",
+        ylab = "part de l'inertie totale (%)")
+# donc pour le présent cas, on peut découper en 6 classes (une classe qui contiendra 2 aires urbaines fufu)
+
+typo_tab_general <- cutree(cah_acp, k = 6)  # k = nombre de classes
+
+acp_tableau_enrichi <- acp_tableau %>%
+  Standar() %>% # nécessaire de standardiser les valeurs du tableau initial
+  as_tibble() %>%
+  mutate(cluster = factor(typo_tab_general, levels = 1:6))
+
+acp_tableau_visu <- acp_tableau_enrichi %>% # nouvelle colonne avec appartenance des lignes i par classe
+  group_by(cluster) %>%
+  summarise_if(is.numeric, mean)
+
+a <- acp_tableau_visu %>%
+  rename(`densité en 2009` = densite_2009,
+         `evo. sp 2009-2018` = sp_09_18,
+         `evo. pop. 1975-1999` = pop_75_99,
+         `evo. pop. 1999-2009` = pop_99_09) %>%
+  pivot_longer(cols = -cluster, names_to = "variables", values_to = "moyenne_cluster") %>%
+  ggplot() +
+  geom_bar(aes(x = moyenne_cluster, y = variables, fill = cluster), stat = "identity", show.legend = FALSE) +
+  scale_fill_tableau(palette = "Tableau 10") +
+  xlab("Moyennes des valeurs standardisées par classe") +
+  theme_julie() +
+  theme(axis.title.y = element_blank()) +
+  labs(caption = "J. Gravier 2020 | LabEx DynamiTe, UMR Géographie-cités\nSources: BPE 2009, 2013, 2018 (Insee), délim. AU 2010 géo. 2019 (Insee), ADMIN EXPRESS géo. 2019 (IGN)",
+       subtitle = "Classe de périodes : CAH (distance euclidienne)") +
+  facet_wrap(~cluster)
+
+# carto
+acp_tableau_enrichi <- acp_tableau_enrichi %>%
+  bind_cols(interpo_grid_2 %>% 
+              filter(sp_09_18 != 0) %>%
+              mutate(geom = x) %>%
+              st_drop_geometry()) %>%
+  st_as_sf(crs = 2154)
+
+b <- ggplot() +
+  geom_sf(data = france, fill = "grey98", color = "grey50", size = 0.3) +
+  geom_sf(data = acp_tableau_enrichi, aes(fill = cluster), show.legend = FALSE, size = 0.2) +
+  scale_fill_tableau(palette = "Tableau 10") +
+  ggspatial::annotation_scale(location = "tr",  width_hint = 0.2) +
+  theme_igray() +
+  theme(axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank()) +
+  ggtitle("Caractérisation de l'évolution des services publics des aires urbaines")
+
+b / a + plot_layout(heights = c(2, 1))
+
+ggsave(filename = "aires_urbaines_acp_interpolation.png", plot = last_plot(), type = "cairo",
+       path = "BPE/sorties/acp_cah/figures/changement_au", dpi = 300,  width = 25, height = 30, units = "cm")
 
